86258b07aa93df5568a771a577b43395130c6bc4
Henry <hprice@merkleinc.com>
2023-11-05 15:17:24 +0000

    Stage 2

---
+++ react-henry/app/components/Navigation.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Navigation() {
+  return (
+    <nav>
+        <ul className=''>
+            <li><a href="/">Home</a></li>
+            <li><a href="/about">About</a></li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Navigation
