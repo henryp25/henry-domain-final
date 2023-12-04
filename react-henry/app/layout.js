f598ccb14fb1f2e6a2bb77e7cd9c6006cb9eb367
Henry <hprice@merkleinc.com>
2023-12-04 20:45:05 +0000

    Final push for GTM

--- react-henry/app/layout.js
+++ react-henry/app/layout.js
@@ -18,21 +18,18 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Script>
-        dangerouslySetInnerHTML={{
-                  __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                  })(window,document,'script','dataLayer','GTM-K6BFSLDH');`,
-                }}
-      </Script>
+      <script
+            dangerouslySetInnerHTML={{
+              __html:
+                `(function(w,l){` +
+                `w[l] = w[l] || [];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});` +
+                `})(window,'dataLayer');`,
+            }}
+          />
+        <Script src={`https://www.googletagmanager.com/gtm.js?id=GTM-K6BFSLDH`} />
       <body>
-
           <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K6BFSLDH"
           height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-
-
       <Navigation />
       {children}
       </body>
