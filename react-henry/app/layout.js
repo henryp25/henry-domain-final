1d1dc6634a281f6c828d70051fc91c162daa51e1
Henry <hprice@merkleinc.com>
2023-12-04 20:34:21 +0000

    adding iframe

--- react-henry/app/layout.js
+++ react-henry/app/layout.js
@@ -29,6 +29,10 @@ export default function RootLayout({ children }) {
       </Script>
       <body>
 
+          <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K6BFSLDH"
+          height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+
+
       <Navigation />
       {children}
       </body>
