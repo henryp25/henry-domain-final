a4ffa5916e550e3a115891320efe8e00ae55f1c4
Henry <hprice@merkleinc.com>
2024-06-17 22:03:58 +0100

    Adding welcome message

--- react-henry/app/components/Onpage/HeaderBanner.js
+++ react-henry/app/components/Onpage/HeaderBanner.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import '../../../styles/banner.css'
+import Image from 'next/image'
 
 function HeaderBanner() {
 
 
   return (
     <div className='banner'>
-        <h1 className="text-6xl font-bold text-center">Welcome to Henrys Site </h1>
+        <h1 className="text-6xl font-bold text-center"> Welcome to my testing domain</h1>
+
     </div>
   )
 }
